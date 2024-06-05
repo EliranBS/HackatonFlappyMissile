c7a4d39c23c5080b664f42008e79928ef0658653
yadinamsalem <81170874+yadinamsalem@users.noreply.github.com>
2024-06-05 15:41:04 +0300

    some fixes

--- Scene/GameManager.gd
+++ Scene/GameManager.gd
@@ -10,20 +10,24 @@ var game_state
 @onready var game_manager = $"."
 @onready var fade = $"../Fade" as Fade
 @onready var ui = $"../UI" as UI
+@onready var misslie = $"../Misslie" as Misslie
 
 var points = 0
+var misslie_timer = null
 var current_level = 1
 
 func _ready():
 	game_state = GameState.IDLE    
 	bird.game_started.connect(on_game_started)
 	pipe_spawner.bird_crashed.connect(end_game)
+	misslie.bird_crashed.connect(end_game)
 	ground.bird_crashed.connect(end_game)
 	pipe_spawner.point_scored.connect(point_scored)
 
 func on_game_started():
 	game_state = GameState.RUNNING
 	pipe_spawner.start_spawning_pipes()
+	misslie.start_misslie_spawning()
 	
 func end_game():
 	if fade != null: 
@@ -31,6 +35,7 @@ func end_game():
 	bird.kill()
 	pipe_spawner.stop()
 	ground.stop()
+	misslie.stop_misslie_spawning()
 	ui.on_game_over()
 
 func point_scored():
@@ -40,16 +45,32 @@ func point_scored():
 	if points % 5 == 0:
 		current_level += 1
 		bird.gravity = bird.gravity * 1.3
-		start_new_level(1000, current_level, points)
+		start_new_level(bird.gravity, current_level, points)
 
 func start_new_level(new_gravity, new_level, points):
 	bird.gravity = new_gravity
 	points = points
 	ui.update_points(points)
 	ui.show_level_up_message(new_level)
-	#reset_game_state()
+
+#func start_misslie_spawning():
+	#misslie_timer = Timer.new()
+	#misslie_timer.wait_time = 2.0
+	#misslie_timer.one_shot = false
+	#misslie_timer.connect("timeout", Callable(self, "_spawn_misslie"))
+	#add_child(misslie_timer)
+	#misslie_timer.start()
 #
-#func reset_game_state():
-	##get_tree().reload_current_scene()  # Reloads the current scene to reset the game state
-	#get_tree()
-	#game_state = GameState.IDLE
+#func stop_misslie_spawning():
+	#if misslie_timer != null:
+		#misslie_timer.stop()
+		#remove_child(misslie_timer)
+		#misslie_timer = null
+		#
+#func _spawn_misslie():
+		#var misslie = misslie_scene.instantiate() as Misslie
+		#var viewport_rect = get_viewport().get_camera_2d().get_viewport_rect()
+		#var half_height = viewport_rect.size.y / 2
+		#misslie.position.x = viewport_rect.end.x
+		#misslie.position.y = randf_range(viewport_rect.size.y * 0.15 - half_height, viewport_rect.size.y * 0.65 - half_height)
+		#add_child(misslie)
