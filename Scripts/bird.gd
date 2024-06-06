674a8286d0452835c5fa6ac60748fc140352c542
Oren <oren.akh@gmail.com>
2024-06-06 09:49:41 +0300

    coomit with oren addition

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -9,7 +9,7 @@ signal game_started
 @onready var animation_player = $AnimationPlayer
 
 var max_speed = 400
-var rotation_speed = 2
+var rotation_speed = 0.2 # oren: it was 2
 
 var is_started = false
 var should_process_input = true
@@ -25,8 +25,11 @@ func _physics_process(delta):
 			game_started.emit()
 			is_started = true
 		jump()
+		await get_tree().create_timer(0.35).timeout
+		animation_player.play("flap_wings")
 		
 	if !is_started:
+		animation_player.play("idle")
 		return
 	velocity.y += gravity * delta
 	
@@ -40,20 +43,22 @@ func _physics_process(delta):
 	
 func jump():
 	velocity.y = jump_force
-	rotation = deg_to_rad(-30)
+	rotation = deg_to_rad(-5) #deg_to_rad(-30)
+	animation_player.play("gas")
 			
 
 func rotate_bird():
 	# Rotate downwards when falling
 	if velocity.y > 0 and rad_to_deg(rotation) < 90:
-		rotation += rotation_speed * deg_to_rad(1)
+		rotation += 10 * rotation_speed * deg_to_rad(1)
 	# Rotate upwards when rising
-	elif velocity.y < 0 and rad_to_deg(rotation) > -30:
+	elif velocity.y < 0 and rad_to_deg(rotation) > -10:
 		rotation -= rotation_speed * deg_to_rad(1)
 
 
 func kill():
 	should_process_input = false
+	$explosion.play()
 	
 func stop():
 	animation_player.stop()
